8a972fb57555af9d25cd2a450527a8e95e7341a2
chaplonglau <chaplonglau@gmail.com>
2015-05-30 10:49:56 -0700

    cartoon

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (array)# code an argument here
   # Your code here
+  for i in 0..array.length-1
+    puts "#{i+1}. #{array[i]}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)# code an argument here
+  s=[]
+  p=""
+  for i in 0..array.length-1
+    p=array[i]+"!"
+    s[i]=p.capitalize
+  end
+  return s
 end
 
-def long_planteer_calls# code an argument here
+def long_planteer_calls(array)# code an argument here
   # Your code here
+  s=false
+  for i in 0..array.length-1
+    if (array[i].length>4)
+      s=true
+    end
+  end
+  return s
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  for i in 0..array.length-1
+    if(cheese_types.include?(array[i])==true)
+      return array[i]
+    end
+  end
+  return nil
 end
