5aebb04106729a4ebe7810e96172f4bcbe7eb90c
Artem Eremin <artross@yandex.ru>
2015-06-10 22:42:16 +0300

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(ary)# code an argument here
   # Your code here
+  ary.each_with_index { |name, i| puts "#{i+1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(ary)# code an argument here
   # Your code here
+  ary.map { |name| name.capitalize + "!"}
 end
 
-def long_planteer_calls# code an argument here
+def long_planteer_calls(ary)# code an argument here
   # Your code here
+  tmp = ary.select { |x| x.length > 4 }
+  !tmp.empty? 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ary)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ary.each { |thing| return thing if cheese_types.include?(thing) }
+  nil
 end
