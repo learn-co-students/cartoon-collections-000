0c224734ed45ac414934757453b3aa1c6c470342
alicht <avilichtschein@gmail.com>
2015-12-02 22:01:17 -0500

    completed lab

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+      puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |power|
+    power.capitalize + ("!")  
+  end
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  word = array.find do |w|
+    w.length > 4
+  end
+
+  if word != nil
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |c|
+    found = cheese_types.find do |cheese_type|
+      c == cheese_type
+    end
+    if found != nil
+      true
+    else
+      false
+    end 
+  end
 end
